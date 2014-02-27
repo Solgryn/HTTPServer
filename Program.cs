b9f44bde36ce9182daacddfe2087309298278313
Jesper Erlandsen <jespererlandsen@hotmail.com>
2014-02-27 11:19:28 +0100

    Simple echo get request

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +11,23 @@ namespace HTTPServer
 {
     class Program
     {
+        static TcpListener serverSocket = new TcpListener(80);
+
         static void Main(string[] args)
         {
+            serverSocket.Start(); //Start listening
+
+            Socket connection = serverSocket.AcceptSocket(); //Accepts a pending connection
+
+            var ns = new NetworkStream(connection);
+            var sr = new StreamReader(ns);
+            var sw = new StreamWriter(ns);
+
+            while (true)
+            {
+                var message = sr.ReadLine(); //Get a request
+                Console.WriteLine("Client (" + connection.RemoteEndPoint + "): " + message);
+            }
         }
     }
 }
