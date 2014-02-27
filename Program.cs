7e015eccebb346be98d2c9e306b994f8b2f89632
Jesper Erlandsen <jespererlandsen@hotmail.com>
2014-02-27 11:58:33 +0100

    Added dynamic response from file

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ namespace HTTPServer
     class Program
     {
         static TcpListener serverSocket = new TcpListener(80);
+        private static readonly string RootCatalog = "c:/temp";
 
         static void Main(string[] args)
         {
@@ -23,32 +24,37 @@ namespace HTTPServer
             var sr = new StreamReader(ns);
             var sw = new StreamWriter(ns);
 
-            var readFirstLine = true;
-            var request = new[] { "" };
+            var readFirstLine = true; //initialize
+            var request = new[] { "" }; //initialize
             while (true)
             {
                 var message = sr.ReadLine(); //Get a request
                 Console.WriteLine("Client (" + connection.RemoteEndPoint + "): " + message);
 
-                //Get request
+                //Get first line from request
                 if (readFirstLine)
                 {
-                    request = message.Split(' ');
-                    readFirstLine = false;
+                    request = message.Split(' '); //split the request up, so the file requested can be read later
+                    readFirstLine = false; //don't read anymore lines
                 }
 
                 //Send a response when request is done
                 if (message == "")
                 {
+                    //Create HTTP header
                     var response = "";
                     response += "HTTP/1.0 200 OK\r\n";
                     response += "Content-Type: text/html\r\n";
                     response += "\r\n";
-                    response += "<html><body>";
-                    response += "<b>Hello, you wrote " + request[1] + "</b>";
-                    response += "</body></html>";
-                    sw.Write(response);
-                    sw.Flush();
+                    //Create body from the specified file
+                    FileStream fileStream = new FileStream(RootCatalog + request[1], FileMode.Open, FileAccess.Read); //New filestream
+                    using (var sr2 = new StreamReader(fileStream)) //use a streamreader to read the filestream
+                    {
+                        response += sr2.ReadToEnd(); //add contents to the response string
+                    }
+
+                    sw.Write(response); //write the response in the streamwriter
+                    sw.Flush(); //send the response
                 }
             }
         }
