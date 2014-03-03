785a5e7318405188959eba8326bb94f2f5ac75c9
Jesper Erlandsen <jespererlandsen@hotmail.com>
2014-03-03 10:23:48 +0100

    Added threading and a not found error message

---
+++ Client.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTTPServer
+{
+    class Client
+    {
+        private Socket connection;
+        private static readonly string RootCatalog = "c:/temp";
+
+        public Client(Socket connection)
+        {
+            this.connection = connection;
+        }
+
+        public void Run()
+        {
+            var response = "";
+            var ns = new NetworkStream(connection);
+            var sr = new StreamReader(ns);
+            var sw = new StreamWriter(ns);
+
+            var readFirstLine = true; //initialize
+            var request = new[] { "" }; //initialize
+            while (true)
+            {
+                var message = sr.ReadLine(); //Get a request
+                if (message != null)
+                {
+                    Console.WriteLine("Client (" + connection.RemoteEndPoint + "): " + message);
+                }
+
+                //Get first line from request
+                if (readFirstLine && message != null)
+                {
+                    request = message.Split(' '); //split the request up, so the file requested can be read later
+                    readFirstLine = false; //don't read anymore lines
+                }
+
+                //Send a response when request is done
+                if (message == "")
+                {
+                    //if (Regex.IsMatch(request, @"GET /[a-z] HTTP/1.0"))
+                    //{
+                        //Does the file exist?
+                        if (File.Exists(RootCatalog + request[1]))
+                        {
+                            //Create HTTP header
+                            response += "HTTP/1.0 200 OK\r\n";
+                            response += "Content-Type: text/html\r\n";
+                            response += "\r\n";
+                            //Create body from the specified file
+                            FileStream fileStream = new FileStream(RootCatalog + request[1], FileMode.Open,
+                                FileAccess.Read);
+                            //New filestream
+                            using (var sr2 = new StreamReader(fileStream)) //use a streamreader to read the filestream
+                            {
+                                response += sr2.ReadToEnd(); //add contents to the response string
+                            }
+                            break;
+                        }
+                        else //If the file doesn't exist, return "404 Not Found"
+                        {
+                            Console.WriteLine("Not found.");
+                            //Create HTTP 404 header
+                            response += "HTTP/1.0 404 Not Found\r\n";
+                            response += "\r\n";
+                            response += "<html>404 not found.</html>";
+                            break;
+                        }
+                    /*}
+                    else
+                    {
+                        //Create HTTP 40 header
+                        var response = "";
+                        response += "HTTP/1.0 400 Illegal request\r\n";
+                        response += "\r\n";
+                    }*/
+                }
+            }
+            try
+            {
+                sw.Write(response); //write the response in the streamwriter
+                sw.Flush(); //send the response
+                Console.WriteLine("Sent response.");
+            }
+            finally
+            {
+                ns.Close();
+                Console.WriteLine("Closed connection.");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,56 +5,35 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace HTTPServer
 {
-    class Program
+    public class Program
     {
-        static TcpListener serverSocket = new TcpListener(80);
-        private static readonly string RootCatalog = "c:/temp";
+        public static int DefaultPort = 80;
+        static TcpListener serverSocket = new TcpListener(DefaultPort);
 
         static void Main(string[] args)
         {
+            //var testString = "GET /file.txt HTTP/1.0";
+
+            //Console.WriteLine(Regex.IsMatch(testString, @"GET /[a-z] HTTP/1.0"));
+
+            //Console.Read();
             serverSocket.Start(); //Start listening
+            Console.WriteLine("Started listening.");
 
-            Socket connection = serverSocket.AcceptSocket(); //Accepts a pending connection
-
-            var ns = new NetworkStream(connection);
-            var sr = new StreamReader(ns);
-            var sw = new StreamWriter(ns);
-
-            var readFirstLine = true; //initialize
-            var request = new[] { "" }; //initialize
             while (true)
             {
-                var message = sr.ReadLine(); //Get a request
-                Console.WriteLine("Client (" + connection.RemoteEndPoint + "): " + message);
-
-                //Get first line from request
-                if (readFirstLine)
+                if (serverSocket.Pending())
                 {
-                    request = message.Split(' '); //split the request up, so the file requested can be read later
-                    readFirstLine = false; //don't read anymore lines
-                }
+                    Socket connection = serverSocket.AcceptSocket(); //Accepts a pending connection
+                    Console.WriteLine("Socket accepted.");
 
-                //Send a response when request is done
-                if (message == "")
-                {
-                    //Create HTTP header
-                    var response = "";
-                    response += "HTTP/1.0 200 OK\r\n";
-                    response += "Content-Type: text/html\r\n";
-                    response += "\r\n";
-                    //Create body from the specified file
-                    FileStream fileStream = new FileStream(RootCatalog + request[1], FileMode.Open, FileAccess.Read); //New filestream
-                    using (var sr2 = new StreamReader(fileStream)) //use a streamreader to read the filestream
-                    {
-                        response += sr2.ReadToEnd(); //add contents to the response string
-                    }
-
-                    sw.Write(response); //write the response in the streamwriter
-                    sw.Flush(); //send the response
+                    var client = new Client(connection);
+                    Task.Factory.StartNew(client.Run); //New client task
                 }
             }
         }
